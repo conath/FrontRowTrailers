55c0e41571e82e0bf7285567c28f59d3b66d7c29
CONATH <christorfer@me.com>
2022-02-12 00:25:05 +0100

    Implement showing basic Movie Metadata

--- TMT Mac/Views/MovieInfoContainerView.swift
+++ TMT Mac/Views/MovieInfoContainerView.swift
@@ -13,6 +13,7 @@ struct MovieInfoContainerView: View {
     @State var frame: GeometryProxy
     
     @State private var animationProgress: CGFloat = 0
+    @State private var shownMovieInfoMetadata: MovieInfo? = nil
     
     private var heightFactor: CGFloat {
         1 - 0.4 * animationProgress
@@ -25,13 +26,16 @@ struct MovieInfoContainerView: View {
     }
     
     var body: some View {
-        VStack {
+        ZStack {
             /// Movie poster image views with fade out and in transition
             FadeInOutPosterView(posterImage: $dataStore.posterImage)
                 .frame(height: frame.size.height * heightFactor)
                 .offset(x: 0, y: yOffset)
                 .rotation3DEffect(Angle(degrees: angle), axis: (x: 0, y: 0.5, z: 0))
                 .onChange(of: dataStore.posterImage) { image in
+                    withAnimation {
+                        shownMovieInfoMetadata = nil
+                    }
                     DispatchQueue.main.asyncAfter(0.3) {
                         guard dataStore.posterImage == image else { return }
                         animationProgress = 0
@@ -39,10 +43,30 @@ struct MovieInfoContainerView: View {
                             guard dataStore.posterImage == image else { return }
                             withAnimation(.easeInOut(duration: 0.5)) {
                                 animationProgress = 1
+                                shownMovieInfoMetadata = dataStore.selectedTrailerModel
                             }
                         }
                     }
                 }
+            if let info = shownMovieInfoMetadata {
+                VStack(alignment: .leading) {
+                    Spacer()
+                        .frame(maxWidth: .infinity, maxHeight: frame.size.height * 0.5)
+                    // Title
+                    Text(info.title)
+                        .font(.largeTitle)
+                        .bold()
+                    // Studio
+                    Text(info.studio)
+                        .font(.title3)
+                    // Trailer length
+                    Text("Trailer: \(info.trailerLength)")
+                        .font(.title3)
+                    Spacer()
+                }
+                .padding()
+                .transition(.opacity)
+            }
         }
     }
 }
